3524b67a85b5565b4d47a504e2330983d7c65876
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-09 22:38:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
 elsif array.length > 2
-  return array[-1].insert(" and ")
+  return array[-1].insert(0, " and ")
 else array.length == 3
   return array.join (" and ")
   
