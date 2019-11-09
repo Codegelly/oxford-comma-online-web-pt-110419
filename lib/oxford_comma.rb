98612a9b47ea71653ba995b7913b88d0bd37a5f7
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-09 22:02:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+# def oxford_comma(array)
+# array.join("")
+# end 
 def oxford_comma(array)
+#array.join("")
+  if array.length == 2
+    return ("")
+elsif array.length > 2 
+  return ("")
+else 
+  return array.join
+  end
+end 
+ 
 
-end
