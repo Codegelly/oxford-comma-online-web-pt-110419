9428e5e25caf8ba376ab05ffe97eb71f47fab872
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-09 22:44:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+# def oxford_comma(array)
+# array.join("")
+# end 
 def oxford_comma(array)
+  if array.length == 2
+    return array.join(" and ")
+elsif array.length > 2
+   array[-1].insert(0, "and ")
+   return array.join(", ")
+else array.length == 3
+  return array.join (" and ")
+  
+  end
+end 
+ 
 
-end
